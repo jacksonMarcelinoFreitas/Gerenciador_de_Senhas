d45c209873a6a0c14c34d415891f71e7ddf8357f
Jackson Freitas <jacksonzitap.mc@gmail.com>
2022-06-12 23:09:38 -0300

    update sql

--- banco_cadastrar_senhas.sql
+++ banco_cadastrar_senhas.sql
@@ -7,19 +7,13 @@ CREATE TABLE Usuario(
 	sobrenome VARCHAR(50)
 );
 
-CREATE TABLE Senha(
-	idSenha INT AUTO_INCREMENT PRIMARY KEY,
-	senha VARCHAR(50)
-);
-
 CREATE TABLE Login(
 	idLogin INT AUTO_INCREMENT PRIMARY KEY,
 	tipo VARCHAR(50),
 	url VARCHAR(50),
+    senha VARCHAR(50),
     fk_idUsuario INT,
-    fk_idSenha INT,
-	FOREIGN KEY (fk_idUsuario) REFERENCES Usuario(idUsuario),
-    FOREIGN KEY (fk_idSenha) REFERENCES Senha(idSenha)
+	FOREIGN KEY (fk_idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 INSERT INTO Usuario (nome, sobrenome) VALUES ("Jackson", "Marcelino de Freitas");
@@ -27,19 +21,15 @@ INSERT INTO Usuario (nome, sobrenome) VALUES ("Alciomar", "Ackley");
 INSERT INTO Usuario (nome, sobrenome) VALUES ("Vitor", "Da Silva");
 
 
-INSERT INTO Login (tipo, url, fk_idUsuario, fk_idSenha) VALUES ("Google", "https://www.google.com/account/about/", 1, 1);
-INSERT INTO Login (tipo, url, fk_idUsuario, fk_idSenha) VALUES ("Amazon", "https://www.amazon.com.br/", 2, 2);
-INSERT INTO Login (tipo, url, fk_idUsuario, fk_idSenha) VALUES ("Behance", "https://www.behance.net/", 3, 3);
-
-INSERT INTO Senha (senha) VALUES ("freitas1457JDK");
-INSERT INTO Senha (senha) VALUES ("amifit123");
-INSERT INTO Senha (senha) VALUES ("123456");
+INSERT INTO Login (tipo, url, senha, fk_idUsuario) VALUES ("Google", "https://www.google.com/account/about/", "jackson12345", 1);
+INSERT INTO Login (tipo, url, senha, fk_idUsuario) VALUES ("Google", "https://www.amazon.com.br/", "AlciomarOlanda", 2);
+INSERT INTO Login (tipo, url, senha, fk_idUsuario) VALUES ("Google", "https://www.behance.net/", "VitaoZinho", 3);
 
 
 SELECT * FROM Usuario; 
 SELECT * FROM Login;
-SELECT * FROM Senha;
 
-DROP TABLE Senha;
-DROP TABLE Login;
 DROP TABLE Usuario;
+DROP TABLE Login;
+drop schema gerenciar_senhas;
+
